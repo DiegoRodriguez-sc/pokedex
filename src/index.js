6bb6ab7f204f252789c674685cdb39a90af51862
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-08-07 22:21:29 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PokedexApp from './PokedexApp';
+import "./styles/styles.css"
+
+ReactDOM.render(
+    <PokedexApp />,
+  document.getElementById('root')
+);
